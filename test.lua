5890f898ecbd924807b62f0c270499a47c3bddfb
Nathan Whitehead <nwhitehe@gmail.com>
2014-12-11 13:09:56 -0800

    writeup

--- test.lua
+++ test.lua
@@ -1,14 +1,24 @@
 local inspect = require('inspect')
 local vida = require('vida')
 
-vida.source([[
+local fast = vida.source('test', [[
+// C interface
 int testConstant;
 int theFunction(int a, int b);
-]],[[
+]],
+[[
+// C implementation
 int testConstant = 101;
 int theFunction(int a, int b) {
     return a + b;
 }
 ]])
 
-print(inspect(vida.sources))
+print(inspect(fast))
+
+print(fast.testConstant)
+
+local x = 3
+local y = 5
+local z = fast.theFunction(x, y)
+print(x, y, z)
