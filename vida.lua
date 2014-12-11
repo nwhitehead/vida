7de9c50b08556e85f8dd518e74a1f7e54f4a4cf0
Nathan Whitehead <nwhitehe@gmail.com>
2014-12-11 13:52:32 -0800

    working...

--- vida.lua
+++ vida.lua
@@ -1,11 +1,21 @@
 local os = require('os')
+local ffi = require('ffi')
 
 local vida = {}
 
-vida.sources = {}
-
-function vida.source(interface, implementation)
-    vida.sources[#vida.sources] = {interface, implementation}
+function vida.source(name, interface, implementation)
+    local fname = os.tmpname() .. name
+    local cname = fname .. ".c"
+    local oname = fname .. ".o"
+    local libname = fname .. ".so"
+    local file = io.open(cname, 'w')
+    file:write(interface)
+    file:write(implementation)
+    file:close()
+    os.execute(string.format('clang -fpic -c %s -o %s', cname, oname))
+    os.execute(string.format('clang -shared %s -o %s', oname, libname))
+    ffi.cdef(interface)
+    return ffi.load(libname)
 end
 
 
